8aaca42bbbdd8e9f88a710df56271a6dc427dcf7
domsangio <domenic_sangiovanni@yahoo.com>
2020-12-08 18:54:04 -0500

    Driver code for kmeans

--- SerialCpp/KMeans/main.cpp
+++ SerialCpp/KMeans/main.cpp
@@ -34,6 +34,7 @@ vector<vector<double>> getRandomCentroids(vector<vector<double>> nodes, int dim)
         }
     }
 
+    // sede
     srand(0);
 
     vector<vector<double>> ret(NUM_CLUSTERS, vector<double>(dim, 0));
@@ -94,14 +95,118 @@ int getIndexOfClosestCentroid(vector<vector<double>> centroids, vector<double> e
     return indexOfClosest;
 }
 
+vector<vector<vector<double>>> runKMeans(vector<vector<double>> elements, int dimensionOfVectors, int numClusters, bool verbose) {
+    // generate random centroids here
+    vector<vector<double>> centroids = getRandomCentroids(elements, dimensionOfVectors);
+
+    if (verbose) {
+        cout << "Random centroids: \n";
+        for (int i = 0; i < centroids.size(); i++) {
+            for (auto j = centroids[i].begin(); j != centroids[i].end(); ++j)
+                cout << *j << ' ';
+            cout << "\n";
+        }
+    }
+
+    // create the clusters now
+    vector<vector<vector<double>>> clusters(numClusters, vector<vector<double>>(elements.size(), vector<double>(dimensionOfVectors, 0)));
+    vector<int> sizes(numClusters, 0);
+    map<vector<double>, int> elementToClusterIndex;     // map(vector<double> => int index of cluster in vector)
+    
+
+    // place the elements into their initial clusters
+    for (int i = 0; i < elements.size(); i++) {
+        // find which centroid its closest to
+        int indexOfClosestCentroid = getIndexOfClosestCentroid(centroids, elements[i], dimensionOfVectors);
+        clusters[indexOfClosestCentroid][sizes[indexOfClosestCentroid]++] = elements[i];
+        elementToClusterIndex.insert(pair<vector<double>, int>(elements[i], indexOfClosestCentroid));
+
+        if (verbose) {
+            cout << "Element " << i << " was found to be closest to index " << indexOfClosestCentroid;
+            cout << "\nELEMENT: ";
+            for (int j = 0; j < dimensionOfVectors; j++) {
+                cout << elements[i][j] << ",";
+            }
+            cout << "\nThis index is mean ";
+            for (int j = 0; j < dimensionOfVectors; j++) {
+                cout << centroids[indexOfClosestCentroid][j] << ",";
+            }
+            cout << "with a distance of " << distanceBetweenVectors(centroids[indexOfClosestCentroid], elements[i], dimensionOfVectors) << "\n";
+        }
+    }
+
+
+    bool convergence = false;
+    while (!convergence) {
+        if (verbose) cout << "Iteration\n";
+        convergence = true;
+        // regenerate the centroids so they are the new means
+        for (int i = 0; i < numClusters; i++) {
+            centroids[i] = getMean(clusters[i], dimensionOfVectors);
+        }
+        // reset sizes
+        for (int i = 0; i < sizes.size(); i++) {
+            sizes[i] = 0;
+        }
+
+        // loop through every element and put them their new respective cluster
+        for (int i = 0; i < elements.size(); i++) {
+            int newIndex = getIndexOfClosestCentroid(centroids, elements[i], dimensionOfVectors);
+            int oldIndex = elementToClusterIndex[elements[i]];
+            if (newIndex != oldIndex) {
+                // separate
+                convergence = false;
+            }
+
+            // either way we add the element in to the "new" cluster
+            clusters[newIndex][sizes[newIndex]++] = elements[i];
+            // elementToClusterIndex.insert(pair<vector<double>, int>(elems[i], newIndex));
+            elementToClusterIndex[elements[i]] = newIndex;
+        }
+    }
+
+    if (verbose) {
+        cout << "Converged!\n";
+        // now we want to print the clusters
+        for (int i = 0; i < numClusters; i++) {
+            cout << "Cluster " << i << "\n";
+            for (int j = 0; j < sizes[i]; j++) {
+                for (auto k = clusters[i][j].begin(); k != clusters[i][j].end(); ++k)
+                    cout << *k << ' ';
+                cout << '\n';
+            }
+        }    
+    }
+
+    if (false) {
+        // write the clusters to a file
+        ofstream output;
+        output.open("kmeans_output.txt");
+        output << "x,y,cluster\n";
+        for (int i = 0; i < numClusters; i++) {
+            for (int j = 0; j < sizes[i]; j++) {
+                for (auto k = clusters[i][j].begin(); k != clusters[i][j].end(); ++k)
+                    output << *k << ',';
+                output << i << '\n';
+            }
+        }  
+
+        output.close();
+    }
+
+    // test to see this map makes sense
+    for (int i = 0; i < elements.size(); i++) {
+        // find min distance 
+        if (getIndexOfClosestCentroid(centroids, elements[i], dimensionOfVectors) != elementToClusterIndex[elements[i]]) {
+            cout << "Mismatch at elem " << i << "\n";
+        }
+    }
+
+    return clusters;
+}
+
 int main() {
     // assume we have vectors here
-    // vector<double> v1 {0, 0};
-    // vector<double> v2 {1, 1};
-    // vector<double> v3 {0.1, 0.1};
-    // vector<double> v4 {0, .5};
-    // vector<double> v5 {.2, 1};
-    // vector<vector<double>> elems {v1, v2, v3, v4, v5};
     vector<vector<double>> elems;
 
     for (int i = 0; i < 1000; i++) {
@@ -112,107 +217,7 @@ int main() {
         elems.push_back(addition);
     }
 
-
-    // generate random centroids here
-    vector<vector<double>> centroids = getRandomCentroids(elems, DIM);
-    cout << "Random centroids: \n";
-    for (int i = 0; i < centroids.size(); i++) {
-        for (auto j = centroids[i].begin(); j != centroids[i].end(); ++j)
-            cout << *j << ' ';
-        cout << "\n";
-    }
-
-
-    // create the clusters now
-
-    vector<vector<vector<double>>> clusters(NUM_CLUSTERS, vector<vector<double>>(elems.size(), vector<double>(DIM, 0)));
-    vector<int> sizes(NUM_CLUSTERS, 0);
-    map<vector<double>, int> elementToClusterIndex;     // map(vector<double> => int index of cluster in vector)
-
-    // place the elements into their initial clusters
-    for (int i = 0; i < elems.size(); i++) {
-        // find which centroid its closest to
-        int indexOfClosestCentroid = getIndexOfClosestCentroid(centroids, elems[i], DIM);
-        clusters[indexOfClosestCentroid][sizes[indexOfClosestCentroid]++] = elems[i];
-        elementToClusterIndex.insert(pair<vector<double>, int>(elems[i], indexOfClosestCentroid));
-
-        cout << "Element " << i << " was found to be closest to index " << indexOfClosestCentroid;
-        cout << "\nELEMENT: ";
-        for (int j = 0; j < DIM; j++) {
-            cout << elems[i][j] << ",";
-        }
-        cout << "\nThis index is mean ";
-        for (int j = 0; j < DIM; j++) {
-            cout << centroids[indexOfClosestCentroid][j] << ",";
-        }
-        cout << "with a distance of " << distanceBetweenVectors(centroids[indexOfClosestCentroid], elems[i], DIM) << "\n";
-    }
-
-    bool convergence = false;
-    while (!convergence) {
-        cout << "Iteration\n";
-        convergence = true;
-        // regenerate the centroids so they are the new means
-        for (int i = 0; i < NUM_CLUSTERS; i++) {
-            centroids[i] = getMean(clusters[i], DIM);
-        }
-        // reset sizes
-        for (int i = 0; i < sizes.size(); i++) {
-            sizes[i] = 0;
-        }
-
-        // loop through every element and put them their new respective cluster
-        for (int i = 0; i < elems.size(); i++) {
-            int newIndex = getIndexOfClosestCentroid(centroids, elems[i], DIM);
-            int oldIndex = elementToClusterIndex[elems[i]];
-            if (newIndex != oldIndex) {
-                // separate
-                convergence = false;
-            }
-
-            // either way we add the element in to the "new" cluster
-            clusters[newIndex][sizes[newIndex]++] = elems[i];
-            // elementToClusterIndex.insert(pair<vector<double>, int>(elems[i], newIndex));
-            elementToClusterIndex[elems[i]] = newIndex;
-        }
-    }
-
-    cout << "Converged!\n";
-    // now we want to print the clusters
-    for (int i = 0; i < NUM_CLUSTERS; i++) {
-        cout << "Cluster " << i << "\n";
-        for (int j = 0; j < sizes[i]; j++) {
-            for (auto k = clusters[i][j].begin(); k != clusters[i][j].end(); ++k)
-                cout << *k << ' ';
-            cout << '\n';
-        }
-    }
-
-    // write the clusters to a file
-    ofstream output;
-    output.open("output.txt");
-
-    output << "x,y,cluster\n";
-  
-    for (int i = 0; i < NUM_CLUSTERS; i++) {
-        for (int j = 0; j < sizes[i]; j++) {
-            for (auto k = clusters[i][j].begin(); k != clusters[i][j].end(); ++k)
-                output << *k << ',';
-            output << i << '\n';
-        }
-    }  
-
-    output.close();
-
-
-    // test to see this map makes sense
-    for (int i = 0; i < elems.size(); i++) {
-        // find min distance 
-        if (getIndexOfClosestCentroid(centroids, elems[i], DIM) != elementToClusterIndex[elems[i]]) {
-            cout << "Mismatch at elem " << i << "\n";
-        }
-    }
-
+    runKMeans(elems, DIM, NUM_CLUSTERS, false);
 
 
     return 0;
